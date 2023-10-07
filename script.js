7a88843279dcb739a80cd50b8e9b9a54aa577737
Margot <146921605+ScorpS00999@users.noreply.github.com>
2023-10-07 17:32:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+"use script";
+
+let a = 4;
+let b = 4;
+let c = 3;
+
+if (a == b) {
+    console.log('egal à b');
+}else if (a == c) {
+    console.log('egal à c');
+} else {
+    console.log("egal a rien");
+}
